2dc08c58eba6241df6612155443fb5a4700e0aed
wecode <umurangamclaire@gmail.com>
2019-03-05 08:38:25 +0200

    deployment to herokuddd

--- app/__init__.py
+++ app/__init__.py
@@ -20,9 +20,7 @@ mail = Mail()
 
 def create_app(config_name):
    app = Flask(__name__)
-   # Creating the app configurations
-   app.config.from_object(config_options[config_name])
-
+   
 
   
    mail.init_app(app)
